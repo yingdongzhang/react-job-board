2524dd9768375b98502d589da06b5de67805b546
Yingdong Zhang <zhangyingdong89@gmail.com>
2018-02-04 17:27:48 +1100

    chore: add a user reducer placeholder

--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,15 +1,4 @@
 function user(state = [], action) {
-    const i = action.index;
-    switch (action.type) {
-        case 'INCREMENT_LIKES':
-            return [
-                ...state.slice(0, i), // before the one we are updating
-                {...state[i], likes: state[i].likes + 1},
-                ...state.slice(i + 1), /// after the one we are updating
-            ];
-        default:
-            return state;
-    }
     return state;
 }
 
